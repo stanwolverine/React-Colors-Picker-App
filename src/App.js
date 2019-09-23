9e7030257f3de721350349c3abf297795ce44cdc
stanwolverine <jitu19nagar@gmail.com>
2019-09-23 16:03:31 +0530

    Fixed a bug while fetching savedPalettes from local storage

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends React.Component {
     super(props);
     const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
     this.state = {
-      palettes: savedPalettes.length > 0 ? savedPalettes : seedColors
+      palettes:
+        savedPalettes && savedPalettes.length > 0 ? savedPalettes : seedColors
     };
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
